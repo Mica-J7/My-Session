48a3bfe79d8494173dc6f15ecceccedef568deaf
Mica <jongeau-m@hotmail.fr>
2025-06-28 13:50:10 +0200

    Added auth middleware and secured all API routes.

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 const exercisesCtrl = require('../controllers/exercises');
 
-router.post('/', exercisesCtrl.createExercise);
-router.put('/:id', exercisesCtrl.modifyExercise);
-router.delete('/:id', exercisesCtrl.deleteExercise);
-router.get('/:id', exercisesCtrl.getOneExercise);
-router.get('/', exercisesCtrl.getAllExercises);
+router.post('/', auth, exercisesCtrl.createExercise);
+router.put('/:id', auth, exercisesCtrl.modifyExercise);
+router.delete('/:id', auth, exercisesCtrl.deleteExercise);
+router.get('/:id', auth, exercisesCtrl.getOneExercise);
+router.get('/', auth, exercisesCtrl.getAllExercises);
 
 module.exports = router;
